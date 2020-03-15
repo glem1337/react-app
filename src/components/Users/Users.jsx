5c4012bc14a9368fd66b74eea8e4193a52bb4722
Evhenii Grigoriev <grigorieffevgeniy@gmail.com>
2020-03-15 20:03:20 +0200

    fetch users from api by axios

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,70 +1,33 @@
 import React from 'react';
 import s from './Users.module.css'
+import * as axios from 'axios';
+import userPlaceholder from '../../assets/images/user.jpg'
+
 
 
 const Users = (props) => {
 
     if (props.usersPage.users.length === 0) {
-        props.setUsers([
-            {
-                id: 1,
-                followed: true,
-                photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTHE4aJfygEoR495XBdYtQ9PZK7gKQ53HKdlCY5um21h5_OHWbu',
-                fullName: 'Dmitriy',
-                status: 'I am a boss',
-                location: {
-                    cityName: 'Kiev',
-                    country: 'Ukraine'
-                }
-            },
-            {
-                id: 2,
-                followed: false,
-                photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTHE4aJfygEoR495XBdYtQ9PZK7gKQ53HKdlCY5um21h5_OHWbu',
-                fullName: 'Andrew',
-                status: 'I am a boss too',
-                location: {
-                    cityName: 'Dnepr',
-                    country: 'Ukraine'
-                }
-            },
-            {
-                id: 3,
-                followed: true,
-                photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTHE4aJfygEoR495XBdYtQ9PZK7gKQ53HKdlCY5um21h5_OHWbu',
-                fullName: 'Oleg',
-                status: 'I am a boss too...',
-                location: {
-                    cityName: 'Primorsk',
-                    country: 'Ukraine'
-                }
-            },
-            {
-                id: 4,
-                followed: false,
-                photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTHE4aJfygEoR495XBdYtQ9PZK7gKQ53HKdlCY5um21h5_OHWbu',
-                fullName: 'Vadim',
-                status: 'I am a boss of the boss',
-                location: {
-                    cityName: 'Lviv',
-                    country: 'Ukraine'
-                }
-            },
-        ])
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+                props.setUsers(response.data.items)
+            })
+
+        
     }
 
     const userElements = props.usersPage.users
         .map(user => {
             return <div className={s.user} key={user.id}>
                 <div className={s.userLeft}>
-                    <img src={user.photoUrl} alt="" />
-                    <div>{user.fullName}</div>
+                    <img src={user.photos.small != null ? user.photos.small : userPlaceholder } alt="" />
+                    <div>{user.name}</div>
                     {user.followed ? <button onClick={() => props.unFollow(user.id)}>Unfollow</button> : <button onClick={() => props.follow(user.id)}>Follow</button>}
                 </div>
                 <div className={s.userRight}>
                     <div>Status: {user.status}</div>
-                    <div>City: {user.location.cityName}</div>
-                    <div>Country: {user.location.country}</div>
+                    {/* <div>City: {user.location.cityName}</div> */}
+                    {/* <div>Country: {user.location.country}</div> */}
                 </div>
             </div>
         })
