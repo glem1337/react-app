6c00f415210d8377ca9619aa554647e488669ee5
Evhenii Grigoriev <grigorieffevgeniy@gmail.com>
2020-03-18 23:03:52 +0200

    remove preloader gif

--- src/components/common/Preloader/Preloader.jsx
+++ src/components/common/Preloader/Preloader.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import preloader from '../../../assets/images/preloader.gif'
 
 
 let Preloader = () => {
     return (
         <div>
-            <img src={preloader} alt=""/>
+            Loading...
         </div>
     )
 }
