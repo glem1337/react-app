cb867dbed847efdc58c5567c4bb77d4f8792833e
Evhenii Grigoriev <grigorieffevgeniy@gmail.com>
2020-05-16 17:36:12 +0300

    reselect

--- src/redux/users/users-selectors.js
+++ src/redux/users/users-selectors.js
@@ -1,8 +1,15 @@
-const getUsers = state => state.usersPage && state.usersPage.users;
+import {createSelector} from "reselect";
+
+const getUsersSelector = state => state.usersPage && state.usersPage.users;
 const getPageSize = state => state.usersPage && state.usersPage.pageSize;
 const getTotalUsersCount = state => state.usersPage && state.usersPage.totalUsersCount;
 const getCurrentPage = state => state.usersPage && state.usersPage.currentPage;
 const getIsFetching = state => state.usersPage && state.usersPage.isFetching;
 const getFollowingInProgress = state => state.usersPage && state.usersPage.followingInProgress;
+
+const getUsers = createSelector(
+    getUsersSelector,
+    users => users.filter(user => true) // some logic
+);
 
 export default {getUsers, getPageSize, getTotalUsersCount, getCurrentPage, getIsFetching, getFollowingInProgress};
